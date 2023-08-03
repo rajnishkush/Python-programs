c6ec3038faf4b0c28d39d8bee5583e5fafea9a8f
Rajnish Kumar <50211337+rajnishkush@users.noreply.github.com>
2023-08-03 11:53:31 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,18 @@
+#Find the Grade of the student:
+marks = float(input("Enter your marks in Python: "))
+if(marks >=90.0):
+    print("You got O")
+elif(marks >= 80.0):
+    print("You got A+")
+elif(marks >= 70.0):
+    print("you got A")
+elif(marks >= 60.0):
+    print("you got B+")
+elif(marks >= 50.0):
+    print("you got B")
+elif(marks >= 45.0):
+    print("you got C")
+elif(marks >= 40.0):
+    print("you got D")
+else:
+    print("Fail")
